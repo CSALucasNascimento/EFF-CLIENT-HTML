0890a538c0fbb94bd9a3a7e58ceb53e68b957fac
Lucas Nascimento <csalucasnascimento@gmail.com>
2018-04-17 17:22:54 +1000

    more pages

--- index.js
+++ index.js
@@ -29,3 +29,14 @@ function includeHTML() {
     }
   }
 }
+
+function myFunction(id) {
+  event.preventDefault();
+  var x = document.getElementById(id);
+  if (x.className.indexOf("w3-show") == -1) {
+      x.className += " w3-show";
+  } else { 
+      x.className = x.className.replace(" w3-show", "");
+  }
+  return false;
+}
